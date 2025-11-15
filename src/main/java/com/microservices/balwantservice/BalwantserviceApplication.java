783dc648d20f0268a49fb504af25c637d3c18c9b
iambalwant <balwantverma55555@gmail.com>
2025-11-16 03:28:07 +0530

    Initial commit

---
+++ src/main/java/com/microservices/balwantservice/BalwantserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.balwantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalwantserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalwantserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservices/balwantservice/BalwantserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.balwantservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalwantserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
